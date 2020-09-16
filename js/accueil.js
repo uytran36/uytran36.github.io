105789f3b2630a36cabfef394a0adede036749a9
Trần Khánh Gia Uy <uytran36@gmail.com>
2020-09-16 22:50:43 +0700

    fix email

--- js/accueil.js
+++ js/accueil.js
@@ -1,27 +1,123 @@
-function checkValidity(aID, aSearchTerm, aMsg){
-    var elem = document.getElementById(aID);
-    var invalid = (elem.value.indexOf(aSearchTerm) < 0);
-    if (invalid) {
-        elem.setAttribute("aria-invalid", "true");
-        updateAlert(aMsg);
-    } else {
-        elem.setAttribute("aria-invalid", "false");
-        updateAlert();
-    }
+function a(aID, aSearchTerm, aMsg) {
+  var elem = document.getElementById(aID);
+  var invalid = elem.value.indexOf(aSearchTerm) < 0;
+  if (invalid) {
+    elem.setAttribute("aria-invalid", "true");
+    updateAlert(aMsg);
+  } else {
+    elem.setAttribute("aria-invalid", "false");
+    updateAlert();
+  }
 }
 
 function updateAlert(msg) {
-    var oldAlert = document.getElementById("alert");
-    if (oldAlert) {
-        document.body.removeChild(oldAlert);
-    }
+  var oldAlert = document.getElementById("alert");
+  var span = document.getElementById("errEmail");
+  if (oldAlert) {
+    span.removeChild(oldAlert);
+  }
 
-    if (msg) {
-       var newAlert = document.createElement("div");
-        newAlert.setAttribute("role", "alert");
-        newAlert.setAttribute("id", "alert");
-        var content = document.createTextNode(msg);
-        newAlert.appendChild(content);
-        document.body.appendChild(newAlert);
-    }
+  if (msg) {
+    var newAlert = document.createElement("div");
+    newAlert.setAttribute("role", "alert");
+    newAlert.setAttribute("id", "alert");
+    var content = document.createTextNode(msg);
+    newAlert.appendChild(content);
+    span.appendChild(newAlert);
+  }
+}
+
+function b(aID, aSearchTerm, aMsg) {
+  var elem = document.getElementById(aID);
+  var invalid = elem.value.indexOf(aSearchTerm) < 0;
+  if (invalid) {
+    elem.setAttribute("aria-invalid", "true");
+    updateAlertB(aMsg);
+  } else {
+    elem.setAttribute("aria-invalid", "false");
+    updateAlertB();
+  }
+}
+
+function updateAlertB(msg) {
+  var oldAlert = document.getElementById("alertB");
+  var span = document.getElementById("errCEmail");
+  if (oldAlert) {
+    span.removeChild(oldAlert);
+  }
+
+  if (msg) {
+    var newAlert = document.createElement("div");
+    newAlert.setAttribute("role", "alert");
+    newAlert.setAttribute("id", "alertB");
+    var content = document.createTextNode(msg);
+    newAlert.appendChild(content);
+    span.appendChild(newAlert);
+  }
+}
+
+function IsValid() {
+  let flag = true;
+  var id_email = "frmEmail";
+  var id_conf_email = "frmConfirmEmail";
+  var id_pass = "frmPassword";
+  var id_username = "frmUsername";
+  var id_birth = "birth";
+
+  let control = document.getElementById(id_email);
+  let error = document.getElementById("errEmail");
+
+  if (control.value === "") {
+    let errorMessage = "! Email n'est pas vide";
+    error.innerText = errorMessage;
+    flag = false;
+  } else {
+    error.innerText = "";
+  }
+
+  control = document.getElementById(id_pass);
+  error = document.getElementById("errPass");
+
+  if (control.value === "") {
+    let errorMessage = "! Mot de passe n'est pas vide";
+    error.innerText = errorMessage;
+    flag = false;
+  } else {
+    error.innerText = "";
+  }
+
+  control = document.getElementById(id_conf_email);
+  error = document.getElementById("errCEmail");
+
+  if (control.value === "") {
+    let errorMessage = "! Email n'est pas vide";
+    error.innerText = errorMessage;
+    flag = false;
+  } else {
+    error.innerText = "";
+  }
+
+  control = document.getElementById(id_username);
+  error = document.getElementById("errUser");
+
+  if (control.value === "") {
+    let errorMessage = "! Le nom d'utilisateur n'est pas vide";
+    error.innerText = errorMessage;
+    flag = false;
+  } else {
+    error.innerText = "";
+  }
+
+  control = document.getElementById(id_birth);
+  error = document.getElementById("errBirth");
+
+  if (control.value === "") {
+    let errorMessage = "! Date de naissance n'est pas vide";
+    error.innerText = errorMessage;
+    flag = false;
+  } else {
+    error.innerText = "";
+  }
+
+  return flag;
 }
---
+++ js/login.js
@@ -0,0 +1,31 @@
+
+function IsValid() {
+    let flag = true;
+    var id_email = "frmUsername";
+    var id_pass = "frmPassword";
+  
+    let control = document.getElementById(id_email);
+    let error = document.getElementById("errEmail");
+  
+    if (control.value === "") {
+      let errorMessage = "! Email n'est pas vide";
+      error.innerText = errorMessage;
+      flag = false;
+    } else {
+      error.innerText = "";
+    }
+  
+    control = document.getElementById(id_pass);
+    error = document.getElementById("errPass");
+  
+    if (control.value === "") {
+      let errorMessage = "! Mot de passe n'est pas vide";
+      error.innerText = errorMessage;
+      flag = false;
+    } else {
+      error.innerText = "";
+    }
+  
+    return flag;
+  }
+  

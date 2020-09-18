0b7152698e513909a5ad700240622535f201dcf0
Trần Khánh Gia Uy <uytran36@gmail.com>
2020-09-18 11:20:22 +0700

    update

---
+++ js/playlist1.js
@@ -0,0 +1,59 @@
+document.getElementById("like1").addEventListener("click", function () {
+  if (document.getElementById("like1").style.color == "red")
+    document.getElementById("like1").style.color = "black";
+  else document.getElementById("like1").style.color = "red";
+});
+
+document.getElementById("like2").addEventListener("click", function () {
+  if (document.getElementById("like2").style.color == "red")
+    document.getElementById("like2").style.color = "black";
+  else document.getElementById("like2").style.color = "red";
+});
+
+document.getElementById("like3").addEventListener("click", function () {
+  if (document.getElementById("like3").style.color == "red")
+    document.getElementById("like3").style.color = "black";
+  else document.getElementById("like3").style.color = "red";
+});
+
+document.getElementById("like4").addEventListener("click", function () {
+  if (document.getElementById("like4").style.color == "red")
+    document.getElementById("like4").style.color = "black";
+  else document.getElementById("like4").style.color = "red";
+});
+
+document.getElementById("like5").addEventListener("click", function () {
+  if (document.getElementById("like5").style.color == "red")
+    document.getElementById("like5").style.color = "black";
+  else document.getElementById("like5").style.color = "red";
+});
+
+document.getElementById("like6").addEventListener("click", function () {
+  if (document.getElementById("like6").style.color == "red")
+    document.getElementById("like6").style.color = "black";
+  else document.getElementById("like6").style.color = "red";
+});
+
+document.getElementById("like7").addEventListener("click", function () {
+  if (document.getElementById("like7").style.color == "red")
+    document.getElementById("like7").style.color = "black";
+  else document.getElementById("like7").style.color = "red";
+});
+
+document.getElementById("like8").addEventListener("click", function () {
+  if (document.getElementById("like8").style.color == "red")
+    document.getElementById("like8").style.color = "black";
+  else document.getElementById("like8").style.color = "red";
+});
+
+document.getElementById("like9").addEventListener("click", function () {
+  if (document.getElementById("like9").style.color == "red")
+    document.getElementById("like9").style.color = "black";
+  else document.getElementById("like9").style.color = "red";
+});
+
+document.getElementById("like10").addEventListener("click", function () {
+    if (document.getElementById("like10").style.color == "red")
+      document.getElementById("like10").style.color = "black";
+    else document.getElementById("like10").style.color = "red";
+  });

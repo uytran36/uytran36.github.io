e4e4b683e61e28e71987c6ce8579918df49c85f5
Trần Khánh Gia Uy <uytran36@gmail.com>
2021-05-18 13:54:37 +0700

    ok

--- js/signup.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('btnSignup').addEventListener('click', 
-function() {
-  if(document.getElementById('frmUsername').value != "" && document.getElementById('frmPassword').value != "")
-    window.location.href = "accueil_login.html";
-})
-  
